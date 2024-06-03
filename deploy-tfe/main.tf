faa768a3bc0551ac912f435bb66a3c37fb8fc147
Simon Lynch <simon.lynch@hashicorp.com>
2024-06-03 15:30:13 +1000

    update template

--- deploy-tfe/main.tf
+++ deploy-tfe/main.tf
@@ -1,28 +1,41 @@
-resource "local_file" "tfe_config_yaml" {
-  content  = templatefile("${path.module}/tfe_config.tpl", {
-    TFE_DATABASE_PASSWORD                                    = var.tfe_database_password
-    TFE_ENCRYPTION_PASSWORD                                  = var.tfe_encryption_password
-    TFE_LICENSE                                              = var.tfe_license
-    TFE_OBJECT_STORAGE_S3_SERVER_SIDE_ENCRYPTION_KMS_KEY_ID  = var.tfe_object_storage_s3_server_side_encryption_kms_key_id
-    TFE_REDIS_PASSWORD                                       = var.tfe_redis_password
-    TFE_CAPACITY_CONCURRENCY                                 = var.tfe_capacity_concurrency
-    TFE_DATABASE_HOST                                        = var.tfe_database_host
-    TFE_DATABASE_NAME                                        = var.tfe_database_name
-    TFE_DATABASE_PARAMETERS                                  = var.tfe_database_parameters
-    TFE_DATABASE_USER                                        = var.tfe_database_user
-    TFE_HOSTNAME                                             = var.tfe_hostname
-    TFE_IACT_SUBNETS                                         = var.tfe_iact_subnets
-    TFE_OBJECT_STORAGE_S3_BUCKET                             = var.tfe_object_storage_s3_bucket
-    TFE_OBJECT_STORAGE_S3_ENDPOINT                           = var.tfe_object_storage_s3_endpoint
-    TFE_OBJECT_STORAGE_S3_REGION                             = var.tfe_object_storage_s3_region
-    TFE_OBJECT_STORAGE_S3_SERVER_SIDE_ENCRYPTION             = var.tfe_object_storage_s3_server_side_encryption
-    TFE_OBJECT_STORAGE_S3_USE_INSTANCE_PROFILE               = var.tfe_object_storage_s3_use_instance_profile
-    TFE_OBJECT_STORAGE_TYPE                                  = var.tfe_object_storage_type
-    TFE_REDIS_HOST                                           = var.tfe_redis_host
-    TFE_REDIS_USE_AUTH                                       = var.tfe_redis_use_auth
-    TFE_REDIS_USE_TLS                                        = var.tfe_redis_use_tls
-    IMAGE_NAME                                               = var.image_name
-  })    
-    filename = "${path.module}/tfe_config.yaml"
+locals {
+
+  tfe_config_yaml = templatefile("${path.module}/template/tfefdo.tpl", {
+    TFE_DATABASE_PASSWORD                                   = var.tfe_database_password
+    TFE_ENCRYPTION_PASSWORD                                 = var.tfe_encryption_password
+    TFE_LICENSE                                             = var.tfe_license
+    TFE_OBJECT_STORAGE_S3_SERVER_SIDE_ENCRYPTION_KMS_KEY_ID = var.tfe_kms_key_id
+    TFE_REDIS_PASSWORD                                      = var.tfe_redis_password
+    TFE_CAPACITY_CONCURRENCY                                = var.tfe_capacity_concurrency
+    TFE_DATABASE_HOST                                       = var.tfe_database_host
+    TFE_DATABASE_NAME                                       = var.tfe_database_name
+    TFE_DATABASE_PARAMETERS                                 = var.tfe_database_parameters
+    TFE_DATABASE_USER                                       = var.tfe_database_user
+    TFE_HOSTNAME                                            = var.tfe_hostname
+    TFE_IACT_SUBNETS                                        = var.tfe_iact_subnets
+    TFE_OBJECT_STORAGE_S3_BUCKET                            = var.s3_tfe_app_bucket_name
+    TFE_OBJECT_STORAGE_S3_ENDPOINT                          = var.tfe_object_storage_s3_endpoint
+    TFE_OBJECT_STORAGE_S3_REGION                            = var.region
+    TFE_OBJECT_STORAGE_S3_SERVER_SIDE_ENCRYPTION            = var.tfe_object_storage_s3_server_side_encryption
+    TFE_OBJECT_STORAGE_S3_USE_INSTANCE_PROFILE              = var.tfe_object_storage_s3_use_instance_profile
+    TFE_OBJECT_STORAGE_TYPE                                 = var.tfe_object_storage_type
+    TFE_REDIS_HOST                                          = var.redis_primary_endpoint
+    TFE_REDIS_USE_AUTH                                      = var.tfe_redis_use_auth
+    TFE_REDIS_USE_TLS                                       = var.tfe_redis_use_tls
+    IMAGE_NAME                                              = "hashicorp/terraform-enterprise"
+    IMAGE_REPOSITORY                                        = "images.releases.hashicorp.com"
+    IMAGE_TAG                                               = var.image_tag
+    REPLICA_COUNT                                           = var.replica_count
+    SERVICE_TYPE                                            = var.service_type
+    METRICS_ENABLE                                          = "true"
+    METRICS_HTTP_PORT                                       = "9090"
+    METRICS_HTTPS_PORT                                      = "9091"
+    PRIVATE_HTTP_PORT                                       = "8080"
+    PRIVATE_HTTPS_PORT                                      = "8081"
+    TLS_CA_CERT_DATA                                        = var.tls_ca_cert_data
+    TLS_CERT_DATA                                           = var.tls_cert_data
+    TLS_KEY_DATA                                            = var.tls_key_data
+  })
+
 
 }
--- deploy-tfe/variables.tf
+++ deploy-tfe/variables.tf
@@ -9,7 +9,7 @@ variable "region" {
 }
 
 variable "cluster_name" {
-  type    = string
+  type = string
 }
 
 variable "cluster_endpoint" {
@@ -35,28 +35,28 @@ variable "cluster_certificate_authority_data" {
 variable "tfe_database_password" {}
 variable "tfe_encryption_password" {}
 variable "tfe_license" {}
-variable "tfe_object_storage_s3_server_side_encryption_kms_key_id" {}
+variable "tfe_kms_key_id" {}
 variable "tfe_redis_password" {}
 variable "tfe_capacity_concurrency" { default = 100 }
-variable "tfe_database_host" { default = "tfepg.c0ynn81zfwhe.ap-northeast-2.rds.amazonaws.com:5432" }
+variable "tfe_database_host" { /*default = "tfe.c0ynn81zfwhe.ap-northeast-2.rds.amazonaws.com:5432"*/ }
 variable "tfe_database_name" { default = "tfepg" }
 variable "tfe_database_parameters" { default = "sslmode=require" }
 variable "tfe_database_user" { default = "tfe" }
 variable "tfe_hostname" { default = "hashicorp.secureaws.net" }
 variable "tfe_iact_subnets" { default = "0.0.0.0/0" }
-variable "tfe_object_storage_s3_bucket" { default = "hashicorp-tfe-s3-common" }
-variable "tfe_object_storage_s3_endpoint" { default = "https://s3.ap-northeast-2.amazonaws.com" }
-variable "tfe_object_storage_s3_region" { default = "ap-northeast-2" }
+variable "s3_tfe_app_bucket_name" { /*default = "hashicorp-tfe-s3-common"*/ }
+variable "tfe_object_storage_s3_endpoint" { default = "https://s3.ap-southeast-2.amazonaws.com" }
+variable "tfe_object_storage_s3_region" { default = "ap-southeast-2" }
 variable "tfe_object_storage_s3_server_side_encryption" { default = "aws:kms" }
 variable "tfe_object_storage_s3_use_instance_profile" { default = "true" }
 variable "tfe_object_storage_type" { default = "s3" }
-variable "tfe_redis_host" { default = "master.dpt-elasticache-cluster-common.aoqw37.apn2.cache.amazonaws.com" }
+variable "redis_primary_endpoint" { default = "master.dpt-elasticache-cluster-common.aoqw37.apn2.cache.amazonaws.com" }
 variable "tfe_redis_use_auth" { default = "true" }
 variable "tfe_redis_use_tls" { default = "true" }
 variable "image_name" { default = "hashicorp/terraform-enterprise" }
 variable "image_repository" { default = "images.releases.hashicorp.com" }
-variable "image_tag" { default = "v202402-1" }
-variable "replica_count" { default = 5 }
+variable "image_tag" { default = "v202405-1" }
+variable "replica_count" { default = 3 }
 variable "service_type" { default = "ClusterIP" }
 variable "metrics_enable" { default = true }
 variable "metrics_http_port" { default = 9090 }
