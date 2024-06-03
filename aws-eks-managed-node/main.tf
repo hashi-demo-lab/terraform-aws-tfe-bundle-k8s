36d2de964de73d1adc9966bf695aec61b460dee5
Simon Lynch <simon.lynch@hashicorp.com>
2024-06-03 12:53:17 +1000

    add eks

---
+++ aws-eks-managed-node/main.tf
@@ -0,0 +1,87 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.13.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.kubernetes_version
+
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.private_subnets
+
+  cluster_endpoint_public_access = true
+  enable_irsa                    = true
+
+  create_cluster_security_group = true
+  create_node_security_group    = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.large"]
+
+      min_size     = 1
+      max_size     = 4
+      desired_size = 2
+      vpc_security_group_ids = [var.vpc_security_group_ids]
+    
+    }
+
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+
+
+/* access_entries = {
+    # One access entry with a policy associated
+    single = {
+      kubernetes_groups = []
+      principal_arn     = var.eks_clusteradmin_arn
+      username          = var.eks_clusteradmin_username
+
+      policy_associations = {
+        single = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  } */
+
+
+} 
+
+data "aws_eks_cluster" "upstream" {
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
+
+}
+
+data "aws_eks_cluster_auth" "upstream_auth" {
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
+}
+
+
+resource "aws_eks_identity_provider_config" "oidc_config" {
+  #depends_on   = [module.eks]
+  cluster_name = module.eks.cluster_name
+
+  oidc {
+    identity_provider_config_name = "tfstack-terraform-cloud"
+    client_id                     = var.tfc_kubernetes_audience
+    issuer_url                    = var.tfc_hostname
+    username_claim                = "sub"
+    groups_claim                  = "terraform_organization_name"
+  }
+}
+
---
+++ aws-eks-managed-node/variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "eks-tfe"
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.28"
+}
+
+variable "tfc_hostname" {
+  type    = string
+  default = "https://app.terraform.io"
+}
+
+variable "tfc_kubernetes_audience" {
+  type = string
+}
+
+variable "eks_clusteradmin_arn" {
+  type = string
+}
+
+variable "eks_clusteradmin_username" {
+  type = string
+}
+
+variable "vpc_security_group_ids" {
+  type = string
+
+}
