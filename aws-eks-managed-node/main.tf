3201194fc6fbc5a1b451de34e4f42cc27b8f361e
Simon Lynch <simon.lynch@hashicorp.com>
2024-06-03 14:21:39 +1000

    add policy for tfe

--- aws-eks-managed-node/main.tf
+++ aws-eks-managed-node/main.tf
@@ -84,3 +84,50 @@ resource "aws_eks_identity_provider_config" "oidc_config" {
     groups_claim                  = "terraform_organization_name"
   }
 }
+
+
+### TFE specific
+
+# get aws account id from datasource
+data "aws_caller_identity" "current" {}
+
+
+data "aws_iam_policy_document" "tf_eks_fdo_for_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["kms:GenerateDataKey", "kms:Decrypt", "s3:*"]
+    resources = ["arn:aws:kms:*:${data.aws_caller_identity.current.account_id}:key/*", "arn:aws:s3:::*"]
+  }
+}
+
+resource "aws_iam_policy" "tf_eks_fdo_for_policy" {
+  name        = "tf_eks_fdo_for_policy"
+  description = "tf_eks_fdo_for_policy"
+  policy      = data.aws_iam_policy_document.tf_eks_fdo_for_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "eksng_s3_kmsrole" {
+  role       = module.eks.cluster_iam_role_name
+  policy_arn = aws_iam_policy.tf_eks_fdo_for_policy.arn
+}
+
+data "aws_iam_policy_document" "tf_eks_fdo_for_cost_estimation_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["pricing:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "tf_eks_fdo_for_cost_estimation_policy" {
+  name        = "tf_eks_fdo_for_cost_estimation_policy"
+  description = "tf_eks_fdo_for_cost_estimation_policy"
+  policy      = data.aws_iam_policy_document.tf_eks_fdo_for_cost_estimation_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "eksng_pricing_role" {
+  role       = module.eks.cluster_iam_role_name
+  policy_arn = aws_iam_policy.tf_eks_fdo_for_cost_estimation_policy.arn
+}
+
+
