e49c0cbfa5c59dc4948d3d68efc1e9e43d6ab98c
Simon Lynch <simon.lynch@hashicorp.com>
2024-06-04 14:25:59 +1000

    update

--- deploy-tfe/pull.sh
+++ deploy-tfe/pull.sh
@@ -1,10 +1,10 @@
 cat "/Users/simon.lynch/git/terraform-aws-tfe-bundle-k8s/files/terraform.hclic" |  docker login --username terraform images.releases.hashicorp.com --password-stdin
 
 
-kubectl create secret docker-registry terraform-enterprise --docker-server=images.releases.hashicorp.com --docker-username=terraform --docker-password=$(cat /Users/simon.lynch/git/terraform-aws-tfe-bundle-k8s/files/terraform.hclic) -n tfefdo
+kubectl create secret docker-registry terraform-enterprise --docker-server=images.releases.hashicorp.com --docker-username=terraform --docker-password=$(cat /Users/simon.lynch/git/terraform-aws-tfe-bundle-k8s/files/terraform.hclic) -n tfe
 
 helm repo add hashicorp https://helm.releases.hashicorp.com
 
 helm template terraform-enterprise hashicorp/terraform-enterprise --namespace tfe --values tfe_config.yaml
 
-helm install terraform-enterprise hashicorp/terraform-enterprise --namespace tfe --values tfe_config.yaml
+helm install --version v1.2.0 terraform-enterprise hashicorp/terraform-enterprise --namespace tfe --values tfe_config.yaml
